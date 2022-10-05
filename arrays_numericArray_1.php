7d28356fa203837e2cd03ca44acf2ac8fcc1ee2a
nopalariq <ariqsyach2004@gmail.com>
2022-10-05 11:12:28 +0700

    05 oktober 2022_numericArray

---
+++ arrays_numericArray_1.php
@@ -0,0 +1,9 @@
+<?php
+    $badang[0] = "keripik pangsit";
+    $badang[1] = "Minuman Jelly";
+    $badang[2] = "Kripik Aneh";
+
+   echo "===Daftar Barang Yang Dijual=== \n";
+   echo "1. $badang[0] \n2. $badang[1] \n3. $badang[2]\n";
+   echo "===============";
+?>
